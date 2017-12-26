ad658759d54d1dd6a8a77c89e58a6b65d31f7552
ggramaize <geoffroy+dev@gramaize.eu>
2017-12-26 22:32:49 +0100

    API refactor. Cleanup crap.

--- tests.c
+++ tests.c
@@ -1,11 +1,12 @@
 //  --------------------------------------------------------------------------
-//  Reference implementation for rfc.zeromq.org/spec:32/Z85
+//  An implementation for rfc.zeromq.org/spec:32/Z85
 //
 //  This implementation provides a Z85 codec as an easy-to-reuse C class 
 //  designed to be easy to port into other languages.
 
 //  --------------------------------------------------------------------------
 //  Copyright (c) 2010-2013 iMatix Corporation and Contributors
+//  Copyright (c) 2017 Geoffroy GRAMAIZE
 //  
 //  Permission is hereby granted, free of charge, to any person obtaining a 
 //  copy of this software and associated documentation files (the "Software"),
@@ -32,8 +33,6 @@
 #include <string.h>
 #include <assert.h>
 
-//  Basic language taken from CZMQ's prelude
-typedef unsigned char byte;
 #define streq(s1,s2,n) (!strncmp ((s1), (s2), (n)))
 
 //  Maps base 256 to base 85
@@ -51,7 +50,7 @@ static char __Z85_encoder [85 + 1] = {
 
 //  Maps base 85 to base 256
 //  We chop off lower 32 and higher 128 ranges
-static byte __Z85_decoder [96] = {
+static uint8_t __Z85_decoder [96] = {
     0x00, 0x44, 0x00, 0x54, 0x53, 0x52, 0x48, 0x00, 
     0x4B, 0x4C, 0x46, 0x41, 0x00, 0x3F, 0x3E, 0x45, 
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
@@ -69,20 +68,20 @@ static byte __Z85_decoder [96] = {
 //  --------------------------------------------------------------------------
 //  Encode a byte array as a string
 
-char* Z85_encode ( const void *in_data, size_t size)
+char* Z85_encode ( const void *in_data, size_t in_data_sz)
 {
     //  Accepts only byte arrays bounded to 4 bytes
-    if (size % 4)
+    if (in_data_sz % 4)
         return NULL;
-
+    
     uint8_t *data = (uint8_t*) in_data;
-    
-    size_t encoded_size = size * 5 / 4;
-    char *encoded = malloc (encoded_size + 1);
-    
-    size_t char_nbr = 0;
+
+    size_t encoded_size = in_data_sz * 5 / 4;
+    char *encoded = (char*) malloc (encoded_size + 1);
+
+    size_t char_nbr = 0; // Current pointer in the 'encoded' array
     uint32_t value = 0;
-    for (size_t byte_nbr = 0; byte_nbr < size; ) {
+    for ( size_t byte_nbr = 0; byte_nbr < in_data_sz; ) {
         //  Accumulate value in base 256 (binary)
         value <<= 8;
 	value += data [byte_nbr++];
@@ -106,53 +105,57 @@ char* Z85_encode ( const void *in_data, size_t size)
 //  Decode an encoded string into a byte array; size of array will be
 //  strlen (string) * 4 / 5.
 
-byte* Z85_decode (char *string)
+int Z85_decode ( const char *in_str, uint8_t **result, size_t *decoded_size)
 {
+
+    size_t in_str_len = strlen( in_str);
+
     //  Accepts only strings bounded to 5 bytes
-    if (strlen (string) % 5)
-        return NULL;
+    if ( in_str_len % 5 )
+        return 1;
     
-    size_t decoded_size = strlen (string) * 4 / 5;
-    byte *decoded = malloc (decoded_size);
+    *decoded_size = in_str_len * 4 / 5;
+    *result = (uint8_t*) malloc (*decoded_size);
 
     size_t byte_nbr = 0;
     uint32_t value = 0;
-    for (size_t char_nbr = 0; char_nbr < strlen (string); ) {
+    for ( size_t char_nbr = 0; char_nbr < in_str_len; ) {
         //  Accumulate value in base 85
-        value = value * 85 + __Z85_decoder [(byte) string [char_nbr++] - 32];
+        value = value * 85 + __Z85_decoder [ (uint8_t)(in_str [char_nbr++]) - 32];
         if (char_nbr % 5 == 0) {
             //  Output value in base 256
             uint32_t divisor = 16777216; // 256 ^ 3, doesn't need to be computed every time.
             while (divisor) {
-                decoded [byte_nbr++] = value / divisor % 256;
+                (*result)[byte_nbr++] = value / divisor % 256;
                 divisor /= 256;
             }
             value = 0;
         }
     }
-    assert (byte_nbr == decoded_size);
-    return decoded;
+    assert (byte_nbr == *decoded_size);
+    return 0;
 }
 
 int main (void)
 {
-    byte test_data_1 [8] = {
+    uint8_t test_data_1 [8] = {
         0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B
     };
-    byte test_data_2 [32] = {
+    uint8_t test_data_2 [32] = {
         0x8E, 0x0B, 0xDD, 0x69, 0x76, 0x28, 0xB9, 0x1D, 
         0x8F, 0x24, 0x55, 0x87, 0xEE, 0x95, 0xC5, 0xB0, 
         0x4D, 0x48, 0x96, 0x3F, 0x79, 0x25, 0x98, 0x77, 
         0xB4, 0x9C, 0xD9, 0x06, 0x3A, 0xEA, 0xD3, 0xB7  
     };
     char *encoded;
-    byte *decoded;
+    unsigned char *decoded = NULL;
+    size_t decoded_len;
 
     encoded = Z85_encode (NULL, 0);
-    decoded = Z85_decode (encoded);
+    Z85_decode (encoded, &decoded, &decoded_len);
     assert (streq (encoded, "", 1));
-    free (decoded);
     free (encoded);
+    free (decoded);
     
     encoded = Z85_encode (test_data_1, 3);
     assert (encoded == NULL);
@@ -160,7 +163,7 @@ int main (void)
     encoded = Z85_encode (test_data_1, sizeof (test_data_1));
     assert (strlen (encoded) == 10);
     assert (streq (encoded, "HelloWorld", 11));
-    decoded = Z85_decode (encoded);
+    Z85_decode (encoded, &decoded, &decoded_len);
     assert (memcmp (test_data_1, decoded, sizeof (test_data_1)) == 0);
     free (decoded);
     free (encoded);
@@ -168,19 +171,19 @@ int main (void)
     encoded = Z85_encode (test_data_2, sizeof (test_data_2));
     assert (strlen (encoded) == 40);
     assert (streq (encoded, "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6", 41));
-    decoded = Z85_decode (encoded);
+    Z85_decode (encoded, &decoded, &decoded_len);
     assert (memcmp (test_data_2, decoded, sizeof (test_data_2)) == 0);
     free (decoded);
     free (encoded);
 
     //  Standard test keys defined by zmq_curve man page
-    byte client_public [32] = { 
+    uint8_t client_public [32] = { 
         0xBB, 0x88, 0x47, 0x1D, 0x65, 0xE2, 0x65, 0x9B, 
         0x30, 0xC5, 0x5A, 0x53, 0x21, 0xCE, 0xBB, 0x5A, 
         0xAB, 0x2B, 0x70, 0xA3, 0x98, 0x64, 0x5C, 0x26, 
         0xDC, 0xA2, 0xB2, 0xFC, 0xB4, 0x3F, 0xC5, 0x18
     };
-    byte client_secret [32] = { 
+    uint8_t client_secret [32] = { 
         0x7B, 0xB8, 0x64, 0xB4, 0x89, 0xAF, 0xA3, 0x67, 
         0x1F, 0xBE, 0x69, 0x10, 0x1F, 0x94, 0xB3, 0x89, 
         0x72, 0xF2, 0x48, 0x16, 0xDF, 0xB0, 0x1B, 0x51, 
@@ -193,13 +196,13 @@ int main (void)
 //     puts (encoded);
     free (encoded);
 
-    byte server_public [32] = { 
+    uint8_t server_public [32] = { 
         0x54, 0xFC, 0xBA, 0x24, 0xE9, 0x32, 0x49, 0x96, 
         0x93, 0x16, 0xFB, 0x61, 0x7C, 0x87, 0x2B, 0xB0, 
         0xC1, 0xD1, 0xFF, 0x14, 0x80, 0x04, 0x27, 0xC5, 
         0x94, 0xCB, 0xFA, 0xCF, 0x1B, 0xC2, 0xD6, 0x52
     };
-    byte server_secret [32] = { 
+    uint8_t server_secret [32] = { 
         0x8E, 0x0B, 0xDD, 0x69, 0x76, 0x28, 0xB9, 0x1D, 
         0x8F, 0x24, 0x55, 0x87, 0xEE, 0x95, 0xC5, 0xB0, 
         0x4D, 0x48, 0x96, 0x3F, 0x79, 0x25, 0x98, 0x77, 
